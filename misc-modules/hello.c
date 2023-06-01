7442de2c3e9bdc1f59f099d9d24d222fbe34222e
isch4196 <isch4196@colorado.edu>
2023-06-01 14:33:37 -0700

    misc-modules: updated hello.c with github name

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -5,15 +5,17 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+#define GITHUB_NAME "isch4196"
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
-	return 0;
+    printk(KERN_ALERT "Hello, world %s\n", GITHUB_NAME);
+    return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+    printk(KERN_ALERT "Goodbye, cruel world %s\n", GITHUB_NAME);
 }
 
 module_init(hello_init);
